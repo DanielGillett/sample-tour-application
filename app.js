cbcc2a09f282bf294417d60bc0c5953e276eb3ec
Daniel Gillett <daniel@danielgillett.com>
2019-12-10 17:31:12 +0000

    Improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
@@ -58,6 +59,14 @@ const limiter = rateLimit({
     message: 'Too many requests from this IP. Please try again in an hour!'
 });
 app.use('/api', limiter);
+
+//Strip web hook (ahead of body parser)
+// use exmpress build in raw body parser
+app.post(
+    '/webhook-checkout',
+    express.raw({ type: 'application/json' }),
+    bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
